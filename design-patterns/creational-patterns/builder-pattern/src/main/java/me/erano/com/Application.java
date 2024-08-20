fbb6303de8b69b883aea076ce8747e2d0594cd0f
Erano01 <muhammedalisert01@gmail.com>
2024-08-20 17:14:56 +0300

    Update

--- design-patterns/creational-patterns/builder-pattern/src/main/java/me/erano/com/Application.java
+++ design-patterns/creational-patterns/builder-pattern/src/main/java/me/erano/com/Application.java
@@ -1,7 +1,8 @@
 package me.erano.com;
 
-//import me.erano.com.example1.*;
-import me.erano.com.example2.*;
+import me.erano.com.example3.CarBuilder;
+import me.erano.com.example3.CarSchemaBuilder;
+import me.erano.com.example3.Director;
 
 public class Application {
 
@@ -15,11 +16,25 @@ public class Application {
 //		System.out.println(endpoint.getUrl());
 		
 //		ex2
-		Endpoint endpoint = new Endpoint
-				.EndpointBuilder("https://freefoodparty.com")
-				.setPath("/leaderboards")
-				.addQueryParam("name", "JR_AIZAMK")
-				.build();
-		System.out.println(endpoint.getUrl());
+//		Endpoint endpoint = new Endpoint
+//				.EndpointBuilder("https://freefoodparty.com")
+//				.setPath("/leaderboards")
+//				.addQueryParam("name", "JR_AIZAMK")
+//				.build();
+//		System.out.println(endpoint.getUrl());
+		
+//		ex3
+		Director director = new Director();
+
+        CarBuilder builder = new CarBuilder();
+        director.buildBugatti(builder);
+        builder.model("Chiron");
+        System.out.println(builder.build());
+
+        CarSchemaBuilder schemaBuilder = new CarSchemaBuilder();
+        director.buildLambo(schemaBuilder);
+        schemaBuilder.engine("90").nbrOfDoors(3);
+        System.out.println(schemaBuilder.build());
+		
 	}
 }
