5850daff77ba43174b6ec2f88a248a64b03fc8df
rokel <muhammedalisert01@gmail.com>
2025-01-03 21:50:40 +0300

    Command Design Pattern examples push

--- design-patterns/behavioral-patterns/command/src/main/java/me/erano/com/Application.java
+++ design-patterns/behavioral-patterns/command/src/main/java/me/erano/com/Application.java
@@ -1,9 +1,36 @@
 package me.erano.com;
 
+import me.erano.com.example2.AddMemberCommand;
+import me.erano.com.example2.Command;
+import me.erano.com.example2.EWSService;
+import me.erano.com.example2.MailTasksRunner;
+
 //Receiver
 //knows how to perform the operations associated with carrying out a request. Any class may serve as a Receiver.
 public class Application {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws InterruptedException{
+
+        //ex1
+
+        //ex2
+        EWSService service = new EWSService();
+        Command c1 = new AddMemberCommand("a@a.com", "spam", service);
+        MailTasksRunner.getInstance().addCommand(c1);
+        Command c2 = new AddMemberCommand("b@b", "spam", service);
+        MailTasksRunner.getInstance().addCommand(c2);
+        Thread.sleep(3000);
+        MailTasksRunner.getInstance().shutdown();
+
+        //ex3
+
+        //ex4
+
+
+        //ex5
+
+
+
+        //java.lang.Runnable interface represents the Command Design Pattern.
     }
 }
