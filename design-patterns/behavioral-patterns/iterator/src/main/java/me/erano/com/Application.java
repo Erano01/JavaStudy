027982910fff6c6100d2effd7453e3843a39ca64
rokel <muhammedalisert01@gmail.com>
2025-01-14 21:10:28 +0300

    Iterator Design Pattern examples push

--- design-patterns/behavioral-patterns/iterator/src/main/java/me/erano/com/Application.java
+++ design-patterns/behavioral-patterns/iterator/src/main/java/me/erano/com/Application.java
@@ -11,13 +11,15 @@ public class Application {
         // java.util.List -> Aggregate interface
         // java.util.ArrayList -> Concrete Aggregate Class
         // java.util.Iterator -> Iterator interface (declares hasNext(), next(), remove())
-        // java.util.ListIterator -> Concrete iterator class that extends Iterator
-
+        // java.util.ListIterator -> Abstract iterator class that extends Iterator interface.
+        // Each Concrete Iterator have decleration in its own aggregate object (inner class)
         List<String> list = new ArrayList<String>();
         list.add("Something1");
         list.add("Something2");
         list.add("Something3");
         // iterator() factory method returns appropriate concrete iterator for that aggregate object
+        // in this scenario, ArrayList factory method that returns iterator calls listIterator() on AbstractList
+        // and that method returns one of ListIterator<E> iterator implementation that exists in AbstractList (inner class)
         Iterator<String> iterator = list.iterator();
         while(iterator.hasNext()) {
             System.out.println(iterator.next());
