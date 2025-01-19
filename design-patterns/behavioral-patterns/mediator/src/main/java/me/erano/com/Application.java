cc79f09abef1c8ad6243618f428d9c4b77c4247e
rokel <muhammedalisert01@gmail.com>
2025-01-19 20:30:36 +0300

    Mediator Design Pattern examples push

--- design-patterns/behavioral-patterns/mediator/src/main/java/me/erano/com/Application.java
+++ design-patterns/behavioral-patterns/mediator/src/main/java/me/erano/com/Application.java
@@ -1,25 +1,13 @@
 package me.erano.com;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javax.swing.*;
 
-public class Application extends javafx.application.Application {
+public class Application {
 
     public static void main(String[] args) {
-        //to execute this application -> mvn clean javafx:run
-        launch();
+        // javax.swing.ButtonGroup & javax.swing.ButtonModel is best example for this pattern.
+        ButtonGroup buttonGroup;
+        ButtonModel buttonModel;
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        var javaVersion = System.getProperty("java.version");
-        var javafxVersion = System.getProperty("javafx.version");
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
 }
