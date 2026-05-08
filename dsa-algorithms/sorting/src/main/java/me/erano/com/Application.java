548ad01fd80c88c59d01d7062c0fcc91b5a7c692
Erano01 <muhammedalisert01@gmail.com>
2026-05-09 02:20:41 +0300

    insertion sort init.

--- dsa-algorithms/sorting/src/main/java/me/erano/com/Application.java
+++ dsa-algorithms/sorting/src/main/java/me/erano/com/Application.java
@@ -2,7 +2,36 @@ package me.erano.com;
 
 public class Application {
     public static void main(String[] args) {
+        // insertion sort (core)
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        InsertionSort.sort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        // merge sort (core)
 
 
+        // quick sort (core)
+
+
+        // heap sort (core)
+
+
+        // bubble sort
+
+
+
+        // selection sort
+
+
+
+        // counting sort (Önemli - Non-comparison Sort O(n) )
+
+
+        // radix sort (Önemli - Non-comparison Sort O(n) )
+
+
+        // bucket sort (Önemli - Non-comparison Sort O(n) )
     }
 }
--- dsa-algorithms/sorting/src/main/java/me/erano/com/InsertionSort.java
+++ dsa-algorithms/sorting/src/main/java/me/erano/com/InsertionSort.java
@@ -2,5 +2,18 @@ package me.erano.com;
 
 public class InsertionSort {
 
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
 
+            // Move elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
