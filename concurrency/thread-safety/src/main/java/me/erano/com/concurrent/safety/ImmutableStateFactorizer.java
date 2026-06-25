67387697e144b7e2d747f069dc2f7a37c5e1643d
Erano01 <muhammedalisert01@gmail.com>
2026-06-26 01:46:47 +0300

    feat: finalizing ImmutableStateFactorizer

--- concurrency/thread-safety/src/main/java/me/erano/com/concurrent/safety/ImmutableStateFactorizer.java
+++ concurrency/thread-safety/src/main/java/me/erano/com/concurrent/safety/ImmutableStateFactorizer.java
@@ -7,10 +7,45 @@ import java.math.BigInteger;
 
 @ThreadSafe
 public class ImmutableStateFactorizer {
-    @GuardedBy("this") private BigInteger lastNumber;
-    @GuardedBy("this") private BigInteger[] lastFactors;
-    @GuardedBy("this") private long hits;
-    @GuardedBy("this") private long cacheHits;
+    @GuardedBy("this")
+    private BigInteger lastNumber;
+    @GuardedBy("this")
+    private BigInteger[] lastFactors;
+    @GuardedBy("this")
+    private long hits;
+    @GuardedBy("this")
+    private long cacheHits;
 
+    public synchronized long getHits() {
+        return hits;
+    }
+
+    public synchronized double getCacheHitRatio() {
+        return (double) cacheHits / (double) hits;
+    }
+
+    public BigInteger[] service(BigInteger input) {
+        return factor(input);
+    }
+
+    private BigInteger[] factor(BigInteger n) {
+        BigInteger i = n;
+        BigInteger[] factors = null;
+        synchronized (this) {
+            ++hits;
+            if (i.equals(lastNumber)) {
+                ++cacheHits;
+                factors = lastFactors.clone();
+            }
+        }
+        if (factors == null) {
+            factors = factor(i);
+            synchronized (this) {
+                lastNumber = i;
+                lastFactors = factors.clone();
+            }
+        }
+        return factors;
+    }
 
 }
